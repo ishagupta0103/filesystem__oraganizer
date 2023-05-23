a1ed6dd74619ba10af2ab726d6a4d794f8e038d4
Isha Gupta <96580290+ishagupta0103@users.noreply.github.com>
2023-05-23 16:34:55 +0530

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,9 @@
+let utility = {}
+utility.types = {
+    videos: ["mp4", "mkv"],
+    pictures:["jpeg","heic","png"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+module.exports = utility;
