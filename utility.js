abc879337a5315849209e7c0ad7b2a2ac5d97138
Isha Gupta <96580290+ishagupta0103@users.noreply.github.com>
2024-01-30 14:16:38 +0530

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,9 +1,9 @@
 let utility = {}
 utility.types = {
     videos: ["mp4", "mkv"],
-    pictures:["jpeg","heic","png"],
+    pictures:["jpeg","heic","png","jpg"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
-    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    documents: ['pdf','docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
     app: ['exe', 'dmg', 'pkg', "deb"]
 }
 module.exports = utility;
